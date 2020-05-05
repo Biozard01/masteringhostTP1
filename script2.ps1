9c39ae2407b4a86038056b17914dfbdf2f9f4abb
Biozard01 <arthur.laforest33@gmail.com>
2020-05-05 19:17:16 +0200

    hello

---
+++ script2.ps1
@@ -0,0 +1,6 @@
+# LAFOREST Arthur
+# Fait le 05/04/2020
+# Se script permet, en fonction d'arguments que vous lui passés :  d'exécuter une action, de lock l'écran et d'éteindre le PC après 5 secondes 
+
+Start-Sleep -s 10
+Restart-Computer
